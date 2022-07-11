e45bf2e4c9e93c03a43db7fe8c961c3c95701051
shubhamkulkarni95 <shu.sun.k@gmail.com>
2022-07-11 21:17:21 +0530

    All Cumcumbar Program Added

---
+++ src/main/java/Jan9_Cucumbar/Jan9_Cucumbar/App.java
@@ -0,0 +1,7 @@
+package Jan9_Cucumbar.Jan9_Cucumbar;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/Jan9_Cucumbar/Jan9_Cucumbar/AppTest.java
@@ -0,0 +1,13 @@
+package Jan9_Cucumbar.Jan9_Cucumbar;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
